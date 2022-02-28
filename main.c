941ecdb7105a4179f72f5f1d6d7e41757d439166
Geovane Ramirez <geovanesilvaramirez@hotmail.com>
2022-02-28 17:50:03 -0300

    commit inicial

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Ola, Geovane!\n");
+    printf("Que bom ver voce aqui novamente!\n");
+
+float n1;
+float n2;
+printf("Digite o primeiro numero: ");
+scanf("%f", &n1);
+
+printf("Digite o segundo numero: ");
+scanf("%f", &n2);
+
+printf("Os numeros digitados foram %.2f e %.2f \n", n1 , n2 );
+
+
+printf("A soma dos dois numeros e: %.2f \n", n1 + n2);
+
+printf("A subtracao entre os dois numeros e: %.2f \n", n1 - n2);
+
+printf("O produto desses dois numeros e: %.2f \n", n1 * n2);
+
+printf("A divisao entre esses dois numeros e: %.2f \n", n1 / n2);
+
+
+
+
+}
